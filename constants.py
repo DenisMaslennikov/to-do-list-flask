71339a0ece24f8f30c21529bd154d24c35da7f40
Denis Maslennikov <ktotom7@yandex.ru>
2024-10-19 12:25:13 +0300

    add constants for task status

--- constants.py
+++ constants.py
@@ -8,3 +8,8 @@ USERNAME_REGEX = re.compile(r"^[a-zA-Z0-9_.+-]+$")
 
 # Максимально возможное количество задач для запроса на получение списка задач
 MAX_AMOUNT_OF_TASKS_TO_DISPLAY = 1000
+
+# Идентификатор статуса задачи "Не выполнено"
+NOT_COMPLETED_TASK_STATUS_ID = 1
+# Идентификатор статуса задачи "Выполнено"
+COMPLETED_TASK_STATUS_ID = 2
